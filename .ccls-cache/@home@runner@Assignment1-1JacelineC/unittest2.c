19c16ddb7b09ff9a1bb681e30715a9b6b562cd5e
jacelinec118 <31042063-jacelinec118@users.noreply.replit.com>
2024-01-19 17:50:12 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Assignment1-1JacelineC/unittest2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int Unit2(void) {
+  printf("Lucinda Pptter \n");
+  printf("6/24/1992 \n");
+  printf("work 000-000-0101 \n");
+  printf("cell 000-000-0189 \n");
+  return 0;
+}
---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void) {
+  //Unit Test 1
+  printf("1 --- \n");
+  
+  printf("Lucinda Potter \n");
+  printf("6/24/1992 \n");
+  printf("work 000-000-0101 \n");
+  printf("cell 000-000-0189 \n");
+  
+  // Unit Test 2
+  printf("2 --- \n");
+
+  printf("John Smith \n");
+  printf("8/15/1978 \n");
+  printf("work 000-111-0101 \n");
+  printf("cell 000-111-0189 \n");
+  
+  //Unit Test 3
+  printf("3 --- \n");
+
+  printf("Chris Johnson \n");
+  printf("12/01/1987 \n");
+  printf("work 000-222-0101 \n");
+  printf("cell 000-222-0189 \n");
+
+  // Unit Test 4
+  printf("4 --- \n");
+
+  printf("Robert Hall \n");
+  printf("2/27/1949 \n");
+  printf("work 000-444-0101 \n");
+  printf("cell 000-444-0189 \n");
+
+  return 0;
+}
